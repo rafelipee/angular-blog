6a565f20c8a0b6213e2c0ec20812bfd3454bb2d3
Rafael Felipe <rafaa.felipee@me.com>
2023-10-17 21:14:46 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,28 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "title": "ESTRÉIA IRON MEN",
+        "description": "Novo Homem de Ferro 'Marvel Studios anuncia novo filme'",
+        "photoCover":"https://variety.com/wp-content/uploads/2013/03/ironman31.jpg?w=1000&h=563&crop=1"
+    },
+    {
+        "id":"2",
+        "title": "O que estava esperando, chegou! Série anunciada no Disney +",
+        "description": "",
+        "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    },
+    {
+        "id":"3",
+        "title": "Novo filme Pantera Negra lançara em breve",
+        "description": "",
+        "photoCover":"https://midias.correiobraziliense.com.br/_midias/jpg/2022/11/09/1200x800/1_pantera_negra_wakanda_para_sempre-26819084.jpg?20221109143843?20221109143843"
+    },
+    {
+        "id":"4",
+        "title": "X-MEN anuncia filme novo em breve",
+        "description": "",
+        "photoCover":"https://s2-techtudo.glbimg.com/xHTjtrXYovWMzXcxhZLoL82bIZQ=/1200x/smart/filters:cover():strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2023/o/o/RWzATXSaG30IWVkdLHwQ/x-men-capa.jpg"
+    }
+
+
+]
